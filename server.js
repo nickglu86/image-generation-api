2263001aa044ad5cfb702da4f0510f94aa82bd09
nickglu86 <enayfls@gmail.com>
2023-06-03 17:08:02 +0300

    server + client + error handling

--- server.js
+++ server.js
@@ -17,16 +17,21 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/dream', async (req, res) => {
-    const prompt = req.body.prompt;
+      try {
+            const prompt = req.body.prompt;
+            const aiResponse = await openai.createImage({
+                  prompt,
+                  n: 1,
+                  size: '1024x1024',
+            });
+      
+            const image = aiResponse.data.data[0].url;
+            res.send({ image });
+      } catch (error) {
+            console.log(error);
+            res.status(500).send( error?.response.data.error.message);
+      }
 
-    const aiResponse = await openai.createImage({
-      prompt,
-      n: 1,
-      size: '1024x1024',
-    });
-
-    const image = aiResponse.data.data[0].url;
-    res.send({ image });
 });
 
 app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
